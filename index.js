bdd8151de8e3253c2152bc30eea99d6a59f53bde
blck69 <cami.zanza2013@gmail.com>
2025-02-23 20:29:46 +0100

    Add files via upload
    inizio

---
+++ index.js
@@ -0,0 +1,14 @@
+const Discord = require ("discord.js")
+const client = new Discord.Client (
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
+)
+
+client.login ("MTM0MzI1ODQ2Njc5MzI5NTk2NA.Gk6ojs.O_kcrq01sNKQKK6ZFdM6yUUR1vkqoGshXgHDdE")
+
+client.on ("ready", () => {
+    console.log ("💚︲Il bot è online")
+})
+
+client.on  ("messageCreate", (message)=>{
+    console.log ("🩵︲Qualcuno ha appena inviato un messaggio")
+})
